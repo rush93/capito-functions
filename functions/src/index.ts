6e52d6001f90f089ab848f8a9acf127deb6d2bb7
yann dardot <yann@donkeycode.com>
2018-09-26 22:53:40 +0200

    firebase init 2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
-
+import * as admin from 'firebase-admin';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+admin.initializeApp();
